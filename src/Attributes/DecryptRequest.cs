23509e04b0141a47741cfed63b80761d8743d145
Alan Moon <alanmoonbase2004@gmail.com>
2023-10-11 10:39:16 -0700

    Add Attempt To Encrypt/Decrypt AdventureLand Data

--- src/Attributes/DecryptRequest.cs
+++ src/Attributes/DecryptRequest.cs
@@ -8,6 +8,7 @@ namespace sodoff.Attributes;
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
 public class DecryptRequest : Attribute, IAsyncActionFilter {
     const string key = "56BB211B-CF06-48E1-9C1D-E40B5173D759";
+    const string land1Key = "A6EE1C3903204be1B6D2B82533AAF1D2";
 
     private readonly string field;
 
@@ -22,6 +23,7 @@ public class DecryptRequest : Attribute, IAsyncActionFilter {
                 // Get field from submitted form and decrypt
                 var fieldValue = form[field].ToString();
                 string result = TripleDES.DecryptUnicode(fieldValue, key);
+                if (form["apiKey"] == "B4E0F71A-1CDA-462a-97B3-0B355E87E0C8") result = TripleDES.DecryptUnicode(fieldValue, land1Key);
                 var dict = form.ToDictionary(x => x.Key, x => x.Value);
                 dict[field] = result;
                 var updatedForm = new FormCollection(dict);
--- src/Attributes/EncryptResponse.cs
+++ src/Attributes/EncryptResponse.cs
@@ -5,6 +5,7 @@ using System.Text;
 namespace sodoff.Attributes;
 public class EncryptResponse : Attribute, IAsyncResultFilter {
     const string key = "56BB211B-CF06-48E1-9C1D-E40B5173D759";
+    const string land1Key = "A6EE1C3903204be1B6D2B82533AAF1D2";
 
     public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next) {
         var originalBodyStream = context.HttpContext.Response.Body;
@@ -18,6 +19,7 @@ public class EncryptResponse : Attribute, IAsyncResultFilter {
             memoryStream.Seek(0, SeekOrigin.Begin);
             var responseBody = await new StreamReader(memoryStream).ReadToEndAsync();
             var result = $"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<string>{TripleDES.EncryptUnicode(responseBody, key)}</string>";
+            if (context.HttpContext.Request.Form["apiKey"] == "B4E0F71A-1CDA-462a-97B3-0B355E87E0C8") result = $"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<string>{TripleDES.EncryptUnicode(responseBody, land1Key)}</string>";
             context.HttpContext.Response.ContentLength = Encoding.UTF8.GetByteCount(result);
             var newBody = new MemoryStream(Encoding.UTF8.GetBytes(result));
             // Override body with encrypted data
