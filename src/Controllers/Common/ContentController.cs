de6246561612afc410113cde9ee057e9d8e78e62
Alan Moon <alanmoonbase2004@gmail.com>
2023-10-26 17:31:47 -0700

    Change Status To 2

--- src/Controllers/Common/ContentController.cs
+++ src/Controllers/Common/ContentController.cs
@@ -1009,13 +1009,10 @@ public class ContentController : Controller {
             AvatarData? avatar = XmlUtil.DeserializeXml<AvatarData>(ctx.Vikings.FirstOrDefault(e => e.Id == request.RequestUserID)?.AvatarSerialized);
             buddiesRes.Add(new Buddy
             {
-                BestBuddy = false,
                 CreateDate = DateTime.Now,
                 UserID = request.RequestUserID,
-                Status = BuddyStatus.PendingApprovalFromOther,
-                Online = true,
-                DisplayName = avatar.DisplayName,
-                OnMobile = false
+                Status = BuddyStatus.PendingApprovalFromSelf,
+                DisplayName = avatar.DisplayName
             });
         }
 
