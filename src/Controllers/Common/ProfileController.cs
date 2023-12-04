0d69728128646711b0c23075a58e4216e634eca2
Alan Moon <alanmoonbase2004@gmail.com>
2023-12-04 13:47:06 -0800

    -Switch to new database -Add WoJS response for ``GetQuestions``

--- src/Controllers/Common/ProfileController.cs
+++ src/Controllers/Common/ProfileController.cs
@@ -55,49 +55,51 @@ public class ProfileController : Controller {
     }
 
     [HttpPost]
-    [Produces("application/xml")]
+    // [Produces("application/xml")]
     [Route("ProfileWebService.asmx/GetQuestions")]
     public IActionResult GetQuestions() {
-		return Ok(new ProfileQuestionData {
-            Lists = new ProfileQuestionList[] {
-                new ProfileQuestionList {
-                    ID = 4,
-                    Questions = new ProfileQuestion[] {
-                        new ProfileQuestion {
-                            CategoryID = 3,
-                            IsActive = "true", // this is a string, which makes me sad
-                            Locale = "en-US",
-                            Ordinal = 1,
-                            ID = 48,
-                            DisplayText = "How Did You Hear About US ?",
-                            Answers = new ProfileAnswer[] {
-                                new ProfileAnswer {
-                                    ID = 320,
-                                    DisplayText = "TV Commercial",
-                                    Locale = "en-US",
-                                    Ordinal = 1,
-                                    QuestionID = 48
-                                },
-                                new ProfileAnswer {
-                                    ID = 324,
-                                    DisplayText = "I bought the RIders Of Berk DVD",
-                                    Locale = "en-US",
-                                    Ordinal = 5,
-                                    QuestionID = 48
-                                },
-                                new ProfileAnswer {
-                                    ID = 325,
-                                    DisplayText = "I bought the Defenders of Berk DVD",
-                                    Locale = "en-US",
-                                    Ordinal = 6,
-                                    QuestionID = 48
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        });
+        return Ok(XmlUtil.ReadResourceXmlString("questiondata"));
+
+		//return Ok(new ProfileQuestionData {
+  //          Lists = new ProfileQuestionList[] {
+  //              new ProfileQuestionList {
+  //                  ID = 4,
+  //                  Questions = new ProfileQuestion[] {
+  //                      new ProfileQuestion {
+  //                          CategoryID = 3,
+  //                          IsActive = "true", // this is a string, which makes me sad
+  //                          Locale = "en-US",
+  //                          Ordinal = 1,
+  //                          ID = 48,
+  //                          DisplayText = "How Did You Hear About US ?",
+  //                          Answers = new ProfileAnswer[] {
+  //                              new ProfileAnswer {
+  //                                  ID = 320,
+  //                                  DisplayText = "TV Commercial",
+  //                                  Locale = "en-US",
+  //                                  Ordinal = 1,
+  //                                  QuestionID = 48
+  //                              },
+  //                              new ProfileAnswer {
+  //                                  ID = 324,
+  //                                  DisplayText = "I bought the RIders Of Berk DVD",
+  //                                  Locale = "en-US",
+  //                                  Ordinal = 5,
+  //                                  QuestionID = 48
+  //                              },
+  //                              new ProfileAnswer {
+  //                                  ID = 325,
+  //                                  DisplayText = "I bought the Defenders of Berk DVD",
+  //                                  Locale = "en-US",
+  //                                  Ordinal = 6,
+  //                                  QuestionID = 48
+  //                              }
+  //                          }
+  //                      }
+  //                  }
+  //              }
+  //          }
+  //      });
     }
     
     [HttpPost]
