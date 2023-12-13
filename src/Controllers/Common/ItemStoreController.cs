f7035575b5d86ff32a7c0b6c47ab2a93624ef57a
Alan Moon <alanmoonbase2004@gmail.com>
2023-12-12 21:02:37 -0800

    kind of just confused

--- src/Controllers/Common/ItemStoreController.cs
+++ src/Controllers/Common/ItemStoreController.cs
@@ -71,7 +71,7 @@ public class ItemStoreController : Controller {
         Announcement testAnnouncement = new Announcement
         {
             AnnouncementID = worldObjectID,
-            AnnouncementText = "[Message]fungus",
+            AnnouncementText = "{Message}(fungus)",
             Description = "fungus amogus",
             StartDate = new DateTime(2023, 12, 11),
             EndDate = new DateTime(2023, 12, 25),
