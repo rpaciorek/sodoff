b192c923a2c88f6f1e8bd51370c341668d967f9a
Alan Moon <alanmoonbase2004@gmail.com>
2023-12-18 18:09:00 -0800

    Change Server To VPS

--- src/Model/DBContext.cs
+++ src/Model/DBContext.cs
@@ -36,7 +36,7 @@ public class DBContext : DbContext {
         else
         {
             string dbPasswd = File.ReadAllText("./dbpasswd.txt");
-            optionsBuilder.UseMySQL($"Server=172.17.0.1;Database=jumpstartdb;Uid=root;Pwd={dbPasswd};Allow User Variables=True").UseLazyLoadingProxies();
+            optionsBuilder.UseMySQL($"Server=restarted.jumpingcrab.com;Port=3306;Database=jumpstartdb;Uid=root;Pwd={dbPasswd};Allow User Variables=True").UseLazyLoadingProxies();
         }
     }
 
