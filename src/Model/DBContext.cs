359b679bab8831d100ae1f243a254ce89dda7cb3
Alan Moon <alanmoonbase2004@gmail.com>
2023-12-18 18:12:59 -0800

    Back To Docker IP

--- src/Model/DBContext.cs
+++ src/Model/DBContext.cs
@@ -36,7 +36,7 @@ public class DBContext : DbContext {
         else
         {
             string dbPasswd = File.ReadAllText("./dbpasswd.txt");
-            optionsBuilder.UseMySQL($"Server=144.126.132.245;Database=jumpstartdb;Uid=root;Pwd={dbPasswd};Allow User Variables=True").UseLazyLoadingProxies();
+            optionsBuilder.UseMySQL($"Server=172.17.0.1;Database=jumpstartdb;Uid=root;Pwd={dbPasswd};Allow User Variables=True").UseLazyLoadingProxies();
         }
     }
 
