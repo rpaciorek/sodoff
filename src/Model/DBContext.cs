d6ef567eb6c199528db6b0e41442c31da44cdf97
Alan Moon <alanmoonbase2004@gmail.com>
2023-12-18 18:04:28 -0800

    Change DB Name To ``jumpstartdb``

--- src/Model/DBContext.cs
+++ src/Model/DBContext.cs
@@ -36,7 +36,7 @@ public class DBContext : DbContext {
         else
         {
             string dbPasswd = File.ReadAllText("./dbpasswd.txt");
-            optionsBuilder.UseMySQL($"Server=172.17.0.1;Database=jumpstartdb-new;Uid=root;Pwd={dbPasswd};Allow User Variables=True").UseLazyLoadingProxies();
+            optionsBuilder.UseMySQL($"Server=172.17.0.1;Database=jumpstartdb;Uid=root;Pwd={dbPasswd};Allow User Variables=True").UseLazyLoadingProxies();
         }
     }
 
