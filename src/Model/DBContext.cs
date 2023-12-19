4bc508a37ab514b95e67f5fadaa9f52c94922fd9
Alan Moon <alanmoonbase2004@gmail.com>
2023-12-18 18:11:48 -0800

    Only IP No Port

--- src/Model/DBContext.cs
+++ src/Model/DBContext.cs
@@ -36,7 +36,7 @@ public class DBContext : DbContext {
         else
         {
             string dbPasswd = File.ReadAllText("./dbpasswd.txt");
-            optionsBuilder.UseMySQL($"Server=144.126.132.245;Port=3306;Database=jumpstartdb;Uid=root;Pwd={dbPasswd};Allow User Variables=True").UseLazyLoadingProxies();
+            optionsBuilder.UseMySQL($"Server=144.126.132.245;Database=jumpstartdb;Uid=root;Pwd={dbPasswd};Allow User Variables=True").UseLazyLoadingProxies();
         }
     }
 
